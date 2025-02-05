19b3fafe86769e24ab093c33414cb24d174014a4
Milton <miltonbarroca@gmail.com>
2025-02-05 16:41:18 -0300

    update Hero component styles and add introductory text

--- meu-portfolio/src/components/hero/Hero.tsx
+++ meu-portfolio/src/components/hero/Hero.tsx
@@ -7,6 +7,11 @@ const Hero = () => {
         <h1>Olá, eu sou</h1>
         <h2>Milton Barroca</h2>
         <h3>Desenvolvedor</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Nostrum dolorem aut sit cumque ipsum dicta.
+            Exercitationem, porro. Veritatis, reiciendis? 
+            Necessitatibus id vel laborum deleniti veniam nam soluta, 
+            molestiae quibusdam velit.</p>
       </div>
     </section>
   );
