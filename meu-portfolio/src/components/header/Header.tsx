7c0416a29775dc266909f676d729144112cd50be
Milton <miltonbarroca@gmail.com>
2025-02-05 15:35:51 -0300

    remove unused README and index.html files; update header styles and structure; add new navigation item and improve main styles

--- meu-portfolio/src/components/header/Header.tsx
+++ meu-portfolio/src/components/header/Header.tsx
@@ -7,6 +7,7 @@ const Header = () => {
             <ul>
                 <li><a href="#about">Sobre</a></li>
                 <li><a href="#projects">Projetos</a></li>
+                <li><a href="#experiencia">Experiências</a></li>
                 <li><a href="#contact">Contato</a></li>
             </ul>
             <button className="nav-button">Botão</button>
