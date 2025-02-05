1bb412e6aba705aff042174e418b84ec15819a1f
Milton <miltonbarroca@gmail.com>
2025-02-05 10:44:35 -0300

    update header styles and improve main content structure; change heading levels and background color

--- meu-portfolio/src/App.tsx
+++ meu-portfolio/src/App.tsx
@@ -5,8 +5,9 @@ function App() {
     <>
       <Header />
       <main>
-        <h2>Milton Barroca</h2>
-        <p>Dev</p>
+        <h1>Milton Barroca</h1>
+        <h2>Dev</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
       </main>
     </>
   );
