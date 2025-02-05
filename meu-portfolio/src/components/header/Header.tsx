323933812de8a240b807e93b418e0025a74638bf
Milton <miltonbarroca@gmail.com>
2025-02-05 11:18:31 -0300

    enhance header styles and structure; add navigation button and update padding

--- meu-portfolio/src/components/header/Header.tsx
+++ meu-portfolio/src/components/header/Header.tsx
@@ -1,17 +1,16 @@
 import "./Header.scss";
 
 const Header = () => {
-  console.log("Header carregado!");
-
   return (
     <header className="header">
       <div className="container">
         <nav>
-          <ul>
-            <li><a href="#about">Sobre</a></li>
-            <li><a href="#projects">Projetos</a></li>
-            <li><a href="#contact">Contato</a></li>
-          </ul>
+            <ul>
+                <li><a href="#about">Sobre</a></li>
+                <li><a href="#projects">Projetos</a></li>
+                <li><a href="#contact">Contato</a></li>
+            </ul>
+            <button className="nav-button">Botão</button>
         </nav>
       </div>
     </header>
