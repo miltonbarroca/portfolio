2a6accafb1e1f63b7842fce0c5755069a82668f0
Milton <miltonbarroca@gmail.com>
2025-02-06 08:37:14 -0300

    working on sidebars

--- meu-portfolio/src/App.tsx
+++ meu-portfolio/src/App.tsx
@@ -1,12 +1,15 @@
 import Header from "./components/header/Header";
 import Hero from "./components/hero/Hero";
+import SidebarLeft from "./components/sidebarleft/SidebarLeft";
 import "./styles/Main.scss";
+
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <SidebarLeft />
     </>
   );
 }
---
+++ meu-portfolio/src/components/sidebarleft/SidebarLeft.tsx
@@ -0,0 +1,11 @@
+import "./SidebarLeft.scss";
+
+const SidebarLeft = () => {
+  return (
+    <aside className="sidebar-left">
+      {/* Conteúdo futuro da sidebar */}
+    </aside>
+  );
+};
+
+export default SidebarLeft;
