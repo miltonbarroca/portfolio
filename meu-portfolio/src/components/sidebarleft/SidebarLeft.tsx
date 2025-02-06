47b96fcda9f907fcf33b5b211bc1d6432c63ef1e
Milton <miltonbarroca@gmail.com>
2025-02-06 09:00:00 -0300

    add social links to SidebarLeft component and update styles for header and hero sections

--- meu-portfolio/src/components/sidebarleft/SidebarLeft.tsx
+++ meu-portfolio/src/components/sidebarleft/SidebarLeft.tsx
@@ -1,9 +1,22 @@
 import "./SidebarLeft.scss";
+import githubIcon from "../../assets/icons/github.svg";
+import linkedinIcon from "../../assets/icons/linkedin.svg";
 
 const SidebarLeft = () => {
   return (
     <aside className="sidebar-left">
-      {/* Conteúdo futuro da sidebar */}
+      <ul className="social-links">
+        <li>
+          <a href="https://github.com/seu-usuario" target="_blank" rel="noopener noreferrer">
+            <img src={githubIcon} alt="GitHub" />
+          </a>
+        </li>
+        <li>
+          <a href="https://linkedin.com/in/seu-usuario" target="_blank" rel="noopener noreferrer">
+            <img src={linkedinIcon} alt="LinkedIn" />
+          </a>
+        </li>
+      </ul>
     </aside>
   );
 };
