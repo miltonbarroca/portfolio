f28ed62c93b366e680885969b68218c3fe27acd4
Milton <miltonbarroca@gmail.com>
2025-02-12 08:44:37 -0300

    added hamburguer menu to improve responsivity

--- meu-portfolio/src/components/header/Header.tsx
+++ meu-portfolio/src/components/header/Header.tsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
 import "./Header.scss";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className="header">
-        <nav className="nav">
-            <ul>
-                <li><a href="#about">Sobre</a></li>
-                <li><a href="#projects">Projetos</a></li>
-                <li><a href="#experiencia">Experiências</a></li>
-                <li><a href="#contact">Contato</a></li>
-            </ul>
-        </nav>
+      <div className="menu-toggle" onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <nav className={menuOpen ? "nav active" : "nav"}>
+        <ul>
+          <li><a href="#about">Sobre</a></li>
+          <li><a href="#projects">Projetos</a></li>
+          <li><a href="#experiencia">Experiências</a></li>
+          <li><a href="#contact">Contato</a></li>
+        </ul>
+      </nav>
     </header>
   );
 };
